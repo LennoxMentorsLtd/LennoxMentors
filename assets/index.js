c747e6a1261b4e6f17227f9eadabb9d9d5f899f3
Samuel Grant <samueljegrant@outlook.com>
2025-10-09 16:28:33 +1300

    chore: clean up styles and improve responsiveness

--- assets/index.js
+++ assets/index.js
@@ -3,25 +3,16 @@ import './images/contact.jpg';
 import './images/home.jpg';
 import './images/mentoring.jpg';
 
+const body = document.querySelector('body');
 const hamburger = document.querySelector('.hamburger');
-const nav_items = document.querySelector('.nav-items');
 
 hamburger.addEventListener('click', () => {
+  body.classList.toggle('nav-open');
   hamburger.classList.toggle('active');
-  nav_items.classList.toggle('open');
 });
 
 hamburger.addEventListener('keydown', (e) => {
   if (e.key === 'Enter' || e.key === ' ') {
     hamburger.click();
   }
-});
-
-nav_items.querySelectorAll('a').forEach(link => {
-  link.addEventListener('click', () => {
-    if (window.innerWidth <= 768) {
-      hamburger.classList.remove('active');
-      nav_items.classList.remove('open');
-    }
-  });
 });
